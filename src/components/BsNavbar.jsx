0db45142367bd090553561700de1d812df951335
gustimb <brizuelagm@outlook.com>
2022-12-16 15:12:13 -0300

    correccion 2da entrega
    se corrigen link de navbar para react-router-dom

--- src/components/BsNavbar.jsx
+++ src/components/BsNavbar.jsx
@@ -1,19 +1,20 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CardWidget from './CardWidget';
+import { Link } from 'react-router-dom';
 
 const BsNavbar = () => {
     return (
         <header>
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">PAWS UP</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/"><img className='NavBarLogo' src="https://res.cloudinary.com/dhn5dbtsr/image/upload/v1671212094/PawsUP-logo2_1_fsqsve.png" alt="PAWS UP logo" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/category/1">Alimentos</Nav.Link>
-                        <Nav.Link href="/category/2">Accesorios</Nav.Link>
-                        <Nav.Link href="/category/3">Higiene y limpieza</Nav.Link>
-                        <Nav.Link href="/category/4">Salud</Nav.Link>
+                        <Nav.Link as={Link} to="/category/1">Alimentos</Nav.Link>
+                        <Nav.Link as={Link} to="/category/2">Accesorios</Nav.Link>
+                        <Nav.Link as={Link} to="/category/3">Higiene y limpieza</Nav.Link>
+                        <Nav.Link as={Link} to="/category/4">Salud</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
