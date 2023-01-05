bb50c762aaa423161549abd9a7e335d3573902b4
gustimb <brizuelagm@outlook.com>
2023-01-05 20:11:37 -0300

    build
    build

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -6,9 +6,9 @@ const CardWidget = () => {
     const { sumaitemsEnCarrito } = useContext(CartContext);
     return (
         <div id="CarritoNavbar">            
-            <span>
-            🛒
-            </span>
+            
+            <img className='imgCarrito' src="https://res.cloudinary.com/dhn5dbtsr/image/upload/v1672959383/cart_sqq6uk.png" alt="simbolo carrito" />
+            
             {
                 sumaitemsEnCarrito() > 0
                     ? sumaitemsEnCarrito()
